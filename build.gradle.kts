7e77a061e512029f970825f8d6587ea887e4dadc
BreninSul <42633769+BreninSul@users.noreply.github.com>
2024-07-09 20:06:56 +0200

    Update software version and dependencies
    The software version has been upgraded to 1.1.4 in build.gradle.kts. Additionally, the dependency 'http-logging-commons' from 'io.github.breninsul' has been updated to version 1.0.5.

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,7 @@ val kotlinVersion = "2.0.0"
 val javaVersion = JavaVersion.VERSION_17
 
 group = "io.github.breninsul"
-version = "1.1.3"
+version = "1.1.4"
 
 java {
     sourceCompatibility = javaVersion
@@ -63,7 +63,7 @@ tasks.compileKotlin {
 
 dependencies {
     compileOnly("org.springframework.boot:spring-boot-starter:${springBootVersion}")
-    api("io.github.breninsul:http-logging-commons:1.0.4")
+    api("io.github.breninsul:http-logging-commons:1.0.5")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
